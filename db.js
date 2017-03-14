ca6fe383550c767a471c64dc8c0a120c60c5b343
Katie McGinley <katie@mcgnly.com>
2017-03-14 17:11:57 +0100

    get and post

--- db.js
+++ db.js
@@ -12,33 +12,40 @@ var sequelize = new Sequelize(keys.dbName, keys.dbUsername, keys.dbPw, {
   }
 });
 
-var Todo = sequelize.define('user', {
-  task: {
-    type: Sequelize.STRING
-  },
-  completed: {//note to self: postgres hates camelcase, put it in quotes when you query for it
-    type: Sequelize.BOOLEAN
-  },
-  dateadded: {
-  	type: Sequelize.DATE,
-  	defaultValue: Sequelize.NOW
+var Todo = sequelize.define('task', 
+  {
+    title: {
+      type: Sequelize.STRING
+    },
+    completed: {//note to self: postgres hates camelcase, put it in quotes when you query for it
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    },
+    dateadded: {
+    	type: Sequelize.DATE,
+    	defaultValue: Sequelize.NOW
+    }
+    // deadline: {
+    // 	type: sequelize.DATE
+    // },
+    // completed: {
+    // 	type: sequelize.DATE
+    // }
+  }, 
+  {
+    freezeTableName: true, // Model tableName will be the same as the model name
+  getterMethods   : {
+      all: function()  { return this.title + ' ' + this.id + ' ' + this.completed }
+    }
   }
-  // deadline: {
-  // 	type: sequelize.DATE
-  // },
-  // completed: {
-  // 	type: sequelize.DATE
-  // }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
+);
 
-// User.sync({force: true}).then(function () {
-//   // Table created
-//   return User.create({
-//     firstName: 'John',
-//     lastName: 'Hancock'
-//   });
-// });
+Todo.sync().then(function () {
+  // Table created
+  // return User.create({
+  //   firstName: 'John',
+  //   lastName: 'Hancock'
+  // });
+});
 
 export default Todo;
--- routes/todos.js
+++ routes/todos.js
@@ -1,19 +1,25 @@
 import Router from 'koa-router';
-import Todo from './db.js';
+import Todo from '../db.js';
 
 const router = new Router()
 
 // const todos = []
 
-router.get('/', async ctx => {
-  ctx.body = todos
+router.get('/', async ctx => {//really this is /todos/
+  await Todo.findAll().then((todos)=>{
+  	ctx.body = JSON.stringify(todos[0].all);
+  })
 })
 
 
 router.post('/', async ctx => {
-    console.log(ctx.request)
+    console.log('the request is: ', ctx.request.body)
   const title = ctx.request.body.title
-  todos.push({title: title, completed: false})
+  // todos.push({title: title, completed: false})
+  Todo.create({title:title})
+  	.then((createdTodo)=>{
+  		console.log('the name of the created todo is: ', createdTodo);
+  	});
   ctx.status = 201
 })
 
