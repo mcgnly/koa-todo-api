8f1630acb0d6db6cef223ee3f8373eaa545f8b92
mcgnly <katie@mcgnly.com>
2017-04-15 16:36:29 +0200

    create user and get all users

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,23 @@
-// import Users from '../models/userDb.js';
-// import Router from 'koa-router';
+import Users from '../models/userDb.js';
+import Router from 'koa-router';
 
-// const router = new Router();
+const router = new Router();
 
-// router.post('/createUser', async ctx => {
-// 	console.log('the request is: ', ctx.request.body)
-// 	const body = ctx.request.body
-// 	const createdUser = await Users.create({name:body.name});
-// 	ctx.body = JSON.stringify(createdUser);
-// 	ctx.status = 201;
-// })
+router.post('/createUser', async ctx => {
+	console.log('the request is: ', ctx.request.body)
+	const body = ctx.request.body
+	const createdUser = await Users.create({
+		name:body.name,
+		password:body.password
+	});
+	ctx.body = JSON.stringify(createdUser);
+	ctx.status = 201;
+})
 
-// router.get('/users', async ctx => {//really this is /todos/
-// 	const users = await Users.findAll();
-//   	ctx.body = JSON.stringify(users[0].all);
-// })
+router.get('/users', async ctx => {//really this is /todos/
+	const users = await Users.findAll();
+  	ctx.body = JSON.stringify(users[0].all);
+})
 
-// export default router
+
+export default router
