ba1057b3c69982598c4f3dab002f51684867d056
mcgnly <katie@mcgnly.com>
2017-04-15 16:36:04 +0200

    move user routes to user file, create todo/toggle

--- routes/todos.js
+++ routes/todos.js
@@ -1,41 +1,34 @@
 import Router from 'koa-router';
 import Todo from '../models/todoDb.js';
-import Users from '../models/userDb.js';
+// import Users from '../models/userDb.js';
 
 const router = new Router();
 
-router.get('/todos', async ctx => {//really this is /todos/
+router.get('/todos', async ctx => {
 	const todos = await Todo.findAll();
   	ctx.body = JSON.stringify(todos[0].all);
 })
 
 router.post('/todos', async ctx => {
 	console.log('the request is: ', ctx.request.body)
-	const title = ctx.request.body
-	const createdTodo = await Todo.create({title:title.title});
-	// console.log('the name of the created todo is: ', createdTodo);
-	ctx.status = 201;
-})
-
-// router.patch()
-
-router.delete('/', async ctx => {
-	const numRowsDeleted = await Todo.destroy({where: {title:{notEmpty: false}}})
-	console.log('number of rows deleted: ', numRowsDeleted)
-})
-
-router.post('/users', async ctx => {
-	console.log('the request is: ', ctx.request.body)
 	const body = ctx.request.body
-	const createdUser = await Users.create({name:body.name});
-	ctx.body = JSON.stringify(createdUser);
-	ctx.status = 201;
+	const createdTodo = await Todo.create({
+		title:body.title,
+		user:body.user
+		// user:'010e6e86-88f3-4aeb-8a97-150dae82f48a'
+	});
+	ctx.status = 201;//means created
 })
 
-router.get('/users', async ctx => {//really this is /todos/
-	const users = await Users.findAll();
-  	ctx.body = users[0];
-})
-
+router.patch('/todos/toggle', async ctx => {
+	const body = ctx.request.body;
+	const task = await Todo.findOne({id:body.id});
+	console.log('toggling ', task.name);
+	const toggledTodo = await Todo.upsert({
+		id:body.id,
+		completed:!task.completed
+	});
+	ctx.status = 200;//means OK
+});
 
 export default router
