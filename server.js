408830cbca84f0f5c0589e5abb0ba6fd5fdfcde9
mcgnly <katie@mcgnly.com>
2017-05-01 16:03:32 +0200

    add cars

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ import bodyparser from 'koa-bodyparser';
 import todos from './routes/todos.js';
 import users from './routes/users.js';
 import Router from 'koa-router';
+import cors from 'koa-cors';
 
 export const app = new Koa();
 const router = new Router();
 
 app.use(bodyparser());
+app.use(cors());
 
 app.use(async (ctx, next) => {
 	try {
